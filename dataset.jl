716f37b926afb1b03cef03f30e1aeeac33942b97
epeters3 <evanpeterson17@gmail.com>
2019-02-04 10:27:34 -0700

    Allow generateDparityData to generate bidirectional data

--- dataset.jl
+++ dataset.jl
@@ -25,28 +25,30 @@ end
 """
 Generates a dataset whose features are a single time series input
 of random bits. The output label is the parity of n arbitrarily delayed
-(but consistent) previous inputs. For example, for `generateDparityData(10, [0,2,5])`,
+(but consistent) previous inputs. For example, for `generateDparityData(10, [0,-2,-5])`,
 the label of each instance would be set to the parity of the current input, the
-input 2 steps back, and the input 5 steps back. 
+input 2 steps back, and the input 5 steps back. Note: When there is not enough data
+at the beginning or end of the set to compute the parity using parityIndices (calculated
+in a naiive way), the label value is 0.0.
 """
 function generateDparityData(numItems::Int, parityIndices::Array{Int})
     data::dataSet = dataSet(Array{dataItem}(undef, numItems))
-    maxParityIndex = maximum(parityIndices)
+    maxParityIndex = maximum(abs.(parityIndices))
     randBits = rand(0.0:1.0, numItems)
 
     for i in 1:numItems;
         label::Float64 = 0.0
-        if maxParityIndex < i
+        if maxParityIndex < i && i + maxParityIndex <= numItems
             # We have enough training examples to find the full parity.
             # Make the label be the parity of the numbers found at parityIndices
             for parityIndex in parityIndices;
-                label += randBits[i - parityIndex]
+                label += randBits[i + parityIndex]
             end
             label = label % 2 == 0 ? 0 : 1
         end
         data.examples[i] = dataItem([randBits[i]], [label])
     end
-    
+
     return data
 end
 
