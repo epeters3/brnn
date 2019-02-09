82ebf3c028fed1e60c8d6e527b405a7011285e70
Tim Whiting <tim@whitings.org>
2019-02-09 10:34:26 -0700

    updates to bptt

--- brnn.jl
+++ brnn.jl
@@ -3,7 +3,6 @@ using Random
 using dataset: dataItem, dataSet
 # Data Structures
 mutable struct forwardLayer
-    net::Array{Float64}
     activations::Array{Float64}
     weights::Array{Float64,2}
     inputSize::Int
@@ -11,11 +10,11 @@ mutable struct forwardLayer
 end
 
 mutable struct recurrentLayer
-    net::Array{Array{Float64}} #outputs needed for each timestep, this is not a 2d-array since we don't know ahead of time one of the dimensions
     activations::Array{Array{Float64}} #activations needed for each timestep, this is not a 2d-array since we don't know ahead of time one of the dimensions
     weights::Array{Float64,2} #weights only needed for one timestep
     inputSize::Int
     outputSize::Int
+    forward::Bool
 end
 
 mutable struct brnnNetwork
@@ -27,55 +26,110 @@ mutable struct brnnNetwork
     outputSize::Int
 end
 
+struct learningParams
+    activation::Function
+    fprimeNet::Function
+    learningRate::Float64
+    τ::Int64
+end
+
+function sigmoidActivation(inputs::Array{Float64})
+    return 1 ./ (1 .+ .^(-inputs))
+end
+
+function sigmoidFprimeNet(activations::Array{Float64})
+    return activations .* (1 .- activations)
+end
+
+function learningParams(learningRate::Float)
+    return learningParams(sigmoidActivation, sigmoidFprimeNet, learningRate)
+end
+
 # Initalize a brnn with i inputs, n hidden nodes, and o output nodes
 function brnnNetwork(i::Int, n::Int, o::Int)
-    forwardRecurrentLayer = recurrentLayer(i, n)
-    backwardRecurrentLayer = recurrentLayer(i, n)
+    forwardRecurrentLayer = recurrentLayer(i, n, true)
+    backwardRecurrentLayer = recurrentLayer(i, n, false)
     outputLayer = forwardLayer(n * 2, o)
     return brnnNetwork(forwardRecurrentLayer, backwardRecurrentLayer, outputLayer, i, n, o)
 end
 
 # Initalize a recurrentLayer with i inputs, and o output nodes
-function recurrentLayer(i::Int, o::Int)
+function recurrentLayer(i::Int, o::Int, forward::Bool)
   # One timestep for now, we will add timesteps as we need to keep track of activations, and not before
-    net = [Array{Float64}(undef, o)]
-    activations = [Array{Float64}(undef, o)]
+    activations = [Array{Float64}(0, o)];
     weights = (rand(o, i + o + 1) .* 2) .- 1 #TODO: See how to get this to clip to [-1:1]
-    return recurrentLayer(net, activations, weights, i, o)
+    return recurrentLayer(activations, weights, i, o, forward)
 end
 
 # Initalize a regular forward layer with i inputs, and o output nodes
 function forwardLayer(i::Int, o::Int)
-    net = Array{Float64}(undef, o)
     activations = Array{Float64}(undef, o)
     weights = (rand(o, i + 1) .* 2) .- 1
-    println(weights)
-    println()
-    return forwardLayer(net, activations, weights, i, o)
+    return forwardLayer(activations, weights, i, o)
 end
 
+function clearActivations(layer::recurrentLayer)
+    layer.activations = [Array{Float64}(0, layer.outputSize)];
+end
 
 # Forward Propagation From Inputs to Outputs
-function propagateForward(network::brnnNetwork, inputs::Array{dataItem}, activation::Function)
-    # TODO: Call propagate forward for the appropriate number of time steps and the number of inputs 
-    propagateForward(network.recurrentForwardsLayer, inputs[0], activation)
-    propagateForward(network.recurrentBackwardsLayer, inputs[0], activation)
-    propagateForward(network.outputLayer, network.recurrentForwardsLayer, network.recurrentBackwardsLayer, activation)
+function propagateForward(network::brnnNetwork, inputs::Array{dataItem}, params::learningParams)
+    # TODO: Call propagate forward for the appropriate number of time steps and the number of inputs
+    clearActivations(network.recurrentForwardsLayer)
+    clearActivations(network.recurrentBackwardsLayer)
+    propagateForward(network.recurrentForwardsLayer, inputs, params)
+    propagateForward(network.recurrentBackwardsLayer, inputs, params)
+    propagateForward(network.outputLayer, network.recurrentForwardsLayer, network.recurrentBackwardsLayer, params)
 end
 
 # Data items are the input to the recurrent layer
-function propagateForward(layer::recurrentLayer, inputs::dataItem, activation::Function)
+function propagateForward(layer::recurrentLayer, inputs::dataItem, params::learningParams)
     # The input to the recurrent layer is the input concatenated with the recurrent layer's previous activation and a bias
-    push!(layer.net, layer.weights * hcat(inputs.features, layer.activations[end], 1))
-    push!(layer.activations, activation(layer.net[end]))
+    if layer.forward 
+        for i in inputs
+            push!(layer.activations, params.activation(layer.weights * hcat(i.features, layer.activations[end], 1)))
+        end
+    else
+        for i in reverse(inputs)
+            push!(layer.activations, params.activation(layer.weights * hcat(i.features, layer.activations[end], 1)))
+        end
+    end
 end
 
 # The outputs of forward and backward recurrent layers are the inputs to the last layer
-function propagateForward(layer::forwardLayer, forwardInputs::recurrentLayer, backwardInputs::recurrentLayer, activation::Function)
-    layer.net = hcat(forwardInputs.activations[end], backwardInputs.activations[end], 1)
-    layer.activation = activation(layer.net)
+function propagateForward(layer::forwardLayer, forwardInputs::recurrentLayer, backwardInputs::recurrentLayer, params::learningParams)
+    layer.activations = params.activation(layer.weights * hcat(forwardInputs.activations[end], backwardInputs.activations[end], 1))
 end
 
+function bptt(network::brnnNetwork, inputs::Array{dataItem}, params::learningParams)
+    bptt(network.outputLayer, network.recurrentForwardsLayer, network.recurrentBackwardsLayer, inputs, params);
+end
+
+function bptt(layer::recurrentLayer, error::Array{Float64}, inputs::Array{dataItem}, params::learningParams)
+    δweights = []
+    layerError = error
+    if layer.forward
+        for i in layer.activations.length - 1:-1:1
+            layerError = transpose(layer.weights) * layerError
+            push!(δweights, layerError .* hcat(layer.activations[i], inputs[i], 1)) 
+        end
+    else
+        for i in layer.activations.length - 1:-1:1
+            layerError = transpose(layer.weights) * layerError
+            push!(δweights, layerError .* hcat(layer.activations[i], inputs[inputs.length - i], 1)) 
+        end
+    end
+    totalδweights = sum(δweights) ./ δweights.length
+    layer.weights += totalδweights;
+end
+
+function bptt(layer::forwardLayer, forwardInputs::recurrentLayer, backwardInputs::recurrentLayer, inputs::Array{dataItem}, params::learningParams)
+    error = (layer.activations .- inputs[end].labels) .* params.fprimenet(layer.activations)
+    δweights = error .* hcat(forwardInputs.activations[end], backwardInputs.activations[end], 1) .* params.learningRate
+    bptt(forwardInputs, error, params);
+    bptt(backwardInputs, error, params);
+    layer.weights .+= δweights;
+end
 
 # Train the network from a dataset
 function learn(network::brnnNetwork, data::dataSet)
